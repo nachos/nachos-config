c22f82b53aa1f0df89b986f25240b40f9a4385f7
Noam Okman <noamokman@gmail.com>
2015-06-28 22:52:26 +0300

    refactor NachosConfig api

--- test/main.spec.js
+++ test/main.spec.js
@@ -6,10 +6,11 @@ var SettingsFile = require('nachos-settings-file');
 
 describe('nachos-config', function () {
   describe('initialize', function () {
-    var config = nachosConfig();
+    var config = new nachosConfig.NachosConfig();
 
     it('should return an instance of SettingsFile', function () {
       expect(config).to.be.an.instanceof(SettingsFile);
+      expect(config).to.be.an.instanceof(nachosConfig.NachosConfig);
     });
 
     describe('globalDefaults property check', function () {
@@ -19,14 +20,19 @@ describe('nachos-config', function () {
 
       it('should have globalDefaults initialized with \'packages\' and \'server\' properties', function () {
         expect(config._options.globalDefaults.packages).to.be.a('string');
-        expect(config._options.globalDefaults.packages).to.be.a('string');
+        expect(config._options.globalDefaults.server).to.be.a('string');
       });
     });
   });
 
   describe('exports', function () {
-    it('should expose a function', function () {
-      expect(nachosConfig).to.be.a('function');
+    it('should expose a nachosConfig object', function () {
+      expect(nachosConfig).to.be.an('object');
+      expect(nachosConfig).to.be.an.instanceof(SettingsFile);
+    });
+
+    it('should expose a nachosConfig ctor', function () {
+      expect(nachosConfig.NachosConfig).to.be.a('function');
     });
   });
 });
